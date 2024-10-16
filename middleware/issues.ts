8ff05b78532f5d7ff015086c1b4e1cd5d008ad14
Peter Zhang <peter@0g.ai>
2024-10-16 15:05:08 +0800

    update issue msg

--- middleware/issues.ts
+++ middleware/issues.ts
@@ -3,10 +3,20 @@ import { GCTX, IssuesEvent } from "../deps.ts";
 
 export default async (event: IssuesEvent, _context: GCTX) => {
   const { issue } = event;
-  await bot.push(
-    `⚠️ <b>New issue:</b> ${issue.title}` +
+  let msg = `⚠️ <b>Issue ${issue.state}:</b> ${issue.title}` +
       `\n` +
-      `\n${issue.body}`,
+      `\n<b>Issue Contents:</b> ${issue.body}`;
+
+  if (issue.assignee) {
+    msg += `\n<b>Assignee:</b> ${issue.assignee.login}`;
+  }
+
+  if (issue.labels !== undefined && issue.labels.length > 0) {
+    msg += `\n<b>Labels:</b> ${issue.labels.map((label) => label.name).join(", ")}`;
+  }
+
+  await bot.push(
+    msg,
     issue.html_url,
   );
 };
